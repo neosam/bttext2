6428a43fd13585a74a3ac9be2a47dd82e3de3325
Simon Goller <neosam@gmail.com>
2015-01-12 19:46:42 +0100

    Basic map implementation

--- btgame.ml
+++ btgame.ml
@@ -2,8 +2,64 @@ type game = {
 	mutable running: bool;
 }
 
+(* Map implementations *)
+type color = int
+let color_black = 0
+let color_red = 1
+let color_green = 2
+let color_yellow = 3
+let color_blue = 4
+let color_magenta = 5
+let color_cyan = 6
+let color_white = 7
+
+
+
+(* The main modules *)
 let init () = {running = true}
 let quit game = game.running <- false
 
 let step game = ()
 let isDone game = not game.running
+
+
+
+(* Define the Map submodule *)
+module Map = struct
+	type trigger = 
+		| None of unit
+		| Func of (game -> unit)
+
+	type field = {
+		mutable ascii: char;
+		mutable fg: color;
+		mutable bg: color;
+		mutable walkable: bool;
+		mutable trigger: trigger;
+	}
+
+	type gameMap = {
+		width: int;
+		height: int;
+		fields: field list;
+	}
+
+
+	let newEmptyField () = {
+		ascii = ' ';
+		fg = color_white;
+		bg = color_black;
+		walkable = true;
+		trigger = None ()
+	}
+
+	let newMap width height = 
+		let size = width * height in
+		let rec createList lst = function
+		| 0 -> lst
+		| x -> createList ((newEmptyField ()) :: lst) (x - 1)
+		in {width = width; height = height; fields = (createList [] size)}
+end;;
+
+
+
--- btgame.mli
+++ btgame.mli
@@ -1,7 +1,35 @@
+(* Main type for game storage *)
 type game
 
+(* Other ingame types *)
+type color
+val color_black : color
+val color_red : color
+val color_green : color
+val color_yellow : color
+val color_blue : color
+val color_magenta : color
+val color_cyan : color
+val color_white : color
+
+
+(* Initialize the game *)
 val init: unit -> game
+(* Stop game *)
 val quit: game -> unit
 
+(* Calculation for one frame *)
 val step: game -> unit
+(* Check if game is finished *)
 val isDone: game -> bool
+
+(* Map implementations *)
+module Map : sig
+	type field
+	type gameMap
+
+	(* newMap width height
+	   create a new map with the given size.
+	   The fields are initialized with default values *)
+	val newMap: int -> int -> gameMap
+end
