4039d005f56e6d6872cabe947a37e849868b73cc
Simon Goller <neosam+github@posteo.de>
2015-01-31 22:28:10 +0100

    Starting logging in main module

--- Main.ml
+++ Main.ml
@@ -1,12 +1,23 @@
 open Btdisplay
 open Btgame
+open Log
 
+let initLog () = begin
+    let logfile = open_out "logfile.txt" in
+    Log.set_log_level Log.DEBUG;
+    Log.set_output logfile;
+end;;
 
-let main () =
+let main () = begin
+    initLog ();
+    Log.info "%s" "Starting";
     let game = Btgame.init () in
     let d = Btdisplay.init game in
     Btdisplay.setup d game;
     Btdisplay.gameloop d;
-    Btdisplay.quit d;;
+    Btdisplay.quit d;
+    Log.info "%s" "Finish";
+end;;
 
 main ()
+
