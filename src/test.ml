4986f56cbf34b50a907d2a3cf86ffc713d2bc7bf
Simon Goller <neosam+github@posteo.de>
2015-06-06 11:37:05 +0200

    Created simple test for btio and cleanup
    Removed the vim swp files, updated .gitignore and cover more btio
    funcitons in the test application.

--- src/test.ml
+++ src/test.ml
@@ -1,7 +1,17 @@
 open Btio
 
+let rec wait_for_escape io =
+    let key = Btio.get_key io in
+    if key = Btio.key_q then io
+    else wait_for_escape io
+
 let main () =
-    let io = Btio.init () in
-    Btio.quit io;;
+    Btio.init () |>
+    Btio.box |>
+    Btio.hline 5 (2, 2) |>
+    Btio.vline 5 (3, 3) |>
+    Btio.print_string None "Asdf" (1, 4) |>
+    wait_for_escape |>
+    Btio.quit;;
 
 main ();;
